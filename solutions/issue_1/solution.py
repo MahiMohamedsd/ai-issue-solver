9c3dbea1e144e234dbfdf28a4162aad67a396181
MahiMohamedsd <168849291+MahiMohamedsd@users.noreply.github.com>
2025-03-24 00:12:16 +0100

    Add solution for issue #1

---
+++ solutions/issue_1/solution.py
@@ -0,0 +1,15 @@
+'''
+Issue #1 Solution
+----------------------------
+
+Analysis:
+def solution():
+    '''Template solution - please modify based on your needs.'''
+    pass
+
+Solution:
+'''
+
+def solution():
+    '''Template solution - please modify based on your needs.'''
+    pass
