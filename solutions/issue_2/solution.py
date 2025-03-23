2554e65d98225b4bcf668967f8918a8c65274f0a
MahiMohamedsd <168849291+MahiMohamedsd@users.noreply.github.com>
2025-03-24 00:15:24 +0100

    Add solution for issue #2

---
+++ solutions/issue_2/solution.py
@@ -0,0 +1,41 @@
+'''
+Issue #2 Solution
+----------------------------
+
+Analysis:
+def solve_problem():
+    '''Template solution - please modify based on your needs.
+    
+    This is a template function that you should modify to solve your specific problem.
+    Add your implementation here.
+    '''
+    # TODO: Implement your solution here
+    pass
+
+def test_solution():
+    '''Test cases for the solution.'''
+    # TODO: Add test cases here
+    pass
+
+if __name__ == "__main__":
+    test_solution()
+
+Solution:
+'''
+
+def solve_problem():
+    '''Template solution - please modify based on your needs.
+    
+    This is a template function that you should modify to solve your specific problem.
+    Add your implementation here.
+    '''
+    # TODO: Implement your solution here
+    pass
+
+def test_solution():
+    '''Test cases for the solution.'''
+    # TODO: Add test cases here
+    pass
+
+if __name__ == "__main__":
+    test_solution()
